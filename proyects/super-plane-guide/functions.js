5caaf9dcfc573cd2c763158aac0becb35112c788
Borja Guerrillo <152048431+BorjaGuerrill@users.noreply.github.com>
2024-09-23 14:08:33 -0400

    update233

---
+++ proyects/super-plane-guide/functions.js
@@ -0,0 +1,14 @@
+function easter(){
+  console.log("Mustang...");
+}
+
+
+function dorito(){
+  var rand = Math.floor(Math.random()* 3);
+  if (rand == 2) {
+    console.log("Hello World!");
+  }
+  if (rand != 2) {
+    console.log("Hello Worldn't!");
+  }
+}
